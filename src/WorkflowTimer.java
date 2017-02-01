14109150fd478205e41a7be82798c96f41b01fb0
Kenny Tang <kennyftang@gmail.com>
2017-02-01 10:42:41 -0600

    Added jar file

--- src/WorkflowTimer.java
+++ src/WorkflowTimer.java
@@ -27,115 +27,79 @@ class WorkflowTimer extends JFrame implements Runnable {
     WorkflowTimer() {
         //Default time value
         timeString = "00:00\t\t";
-        //Panel contains the time as a JLabel
-        JPanel timePanel = new JPanel();
         //Panel contains the task list
         taskPanel = new JPanel();
-        //Panel contains timeLeft and timeRight buttons
-        JPanel buttonPanel = new JPanel();
-        //Set the default text of the label
+        //Set the default text of the label to default time value
         timeText = new JLabel(timeString);
         //Set the default text of the two buttons
         timeRight = new JButton("Start");
         timeLeft = new JButton("Reset");
-        //Do not start timing
-        timing = false;
         //Start timing the next task when timer starts
         newTask = true;
         //This layout is for WorkflowTimer
-        BoxLayout rootLayout = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
-        //This layout is for the TaskPanel
-        BoxLayout taskLayout = new BoxLayout(taskPanel, BoxLayout.Y_AXIS);
-        //This layout is for the button panel, displaying left -> right
-        FlowLayout buttonLayout = new FlowLayout(FlowLayout.LEFT, 10, 5);
-        //This layout is for the time panel and is used to center the label
-        GridBagLayout timeLayout = new GridBagLayout();
+        GridBagLayout rootLayout = new GridBagLayout();
 
-        //Set the font of the time label to helvetica since it looks nicer
-        timeText.setFont(new Font("Helvetica", Font.PLAIN, 48));
-        //Set no vertical gap
+        //Set the font of the time label to sans serif since it looks nicer
+        timeText.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 48));
 
         //Set the layout of WorkflowTimer and all the panels
         this.setLayout(rootLayout);
-        timePanel.setLayout(timeLayout);
-        taskPanel.setLayout(taskLayout);
-        buttonPanel.setLayout(buttonLayout);
-        //Align the time label to the center
+        //Set layout of the taskPanel to vertical box
+        taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));
 
         //Disable the reset button
         timeLeft.setEnabled(false);
-        //ActionListener for the left button
-        timeLeft.addActionListener((ActionEvent e) -> {
-            String currentText = ((JButton) e.getSource()).getText();
-            boolean enabled = ((JButton) e.getSource()).isEnabled();
-            if (enabled)
-                switch (currentText) {
-                    case "Reset":
-                        theTime = 0;
-                        timeString = "00:00\t\t";
-                        timeText.setText(timeString);
-                        this.timeLeft.setEnabled(false);
-                        taskPanel.removeAll();
-                        taskPanel.add(WorkflowTimer.taskListLabel);
-                        break;
-                    case "Lap":
-                        new LapWindow(this);
-                }
-        });
-        //AcitonListener for the right button
-        timeRight.addActionListener((ActionEvent e) -> {
-            String currentText = ((JButton) e.getSource()).getText();
-            switch (currentText) {
-                case "Start":
-                    timeLeft.setText("Lap");
-                    timeLeft.setEnabled(true);
-                    timeRight.setText("Stop");
-                    timing = true;
-                    break;
-                case "Stop":
-                    timeLeft.setText("Reset");
-                    timeRight.setText("Start");
-                    timing = false;
-            }
-        });
+        //ActionListener for each button
+        timeLeft.addActionListener(this::onButton);
+        timeRight.addActionListener(this::onButton);
 
-        //Setup the gridBagLayout
+        //Setup the GridBagLayout Constraints
         GridBagConstraints timeTextC = new GridBagConstraints();
         timeTextC.gridx = 1;
         timeTextC.gridy = 0;
-        timeTextC.gridwidth = 3;
+        timeTextC.gridwidth = 2;
         timeTextC.anchor = GridBagConstraints.CENTER;
-        timeTextC.weighty = 1;
-        timeTextC.weightx = 1;
+        timeTextC.insets = new Insets(10, 10, 10, 10);
 
         GridBagConstraints taskPanelC = new GridBagConstraints();
-        taskPanelC.gridx = 4;
-        taskPanelC.gridy = 0;
-        taskPanelC.gridwidth = 1;
-        taskPanelC.anchor = GridBagConstraints.NORTHEAST;
-        taskPanelC.weighty = 1;
+        taskPanelC.gridx = 5;
         taskPanelC.weightx = 1;
+        taskPanelC.gridy = 0;
+        taskPanelC.anchor = GridBagConstraints.NORTHEAST;
         taskPanelC.insets = new Insets(10, 10, 10, 10);
 
-        //Add all the components to the corresponding containers
-        timePanel.add(timeText, timeTextC);
-        timePanel.add(taskPanel, taskPanelC);
+        GridBagConstraints timeLeftC = new GridBagConstraints();
+        timeLeftC.gridx = 1;
+        timeLeftC.gridy = 1;
+        timeLeftC.anchor = GridBagConstraints.SOUTHWEST;
 
+        GridBagConstraints timeRightC = new GridBagConstraints();
+        timeRightC.gridx = 2;
+        timeRightC.gridy = 1;
+        timeRightC.anchor = GridBagConstraints.SOUTHWEST;
+
+        GridBagConstraints horizontalGlueC = new GridBagConstraints();
+        horizontalGlueC.weightx = 1;
+        horizontalGlueC.gridx = 2;
+        horizontalGlueC.gridwidth = 5;
+
+        //Header for the Task Panel
         taskListLabel = new JLabel("Task List");
         taskListLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
-        taskListLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-
+        taskListLabel.setAlignmentX(Component.RIGHT_ALIGNMENT);
         taskPanel.add(taskListLabel);
-        buttonPanel.add(timeLeft);
-        buttonPanel.add(timeRight);
-        this.add(timePanel);
-        this.add(buttonPanel);
+
+        //Add everything to the frame
+        this.add(timeLeft, timeLeftC);
+        this.add(timeRight, timeRightC);
+        this.add(timeText, timeTextC);
+        this.add(taskPanel, taskPanelC);
 
         //Default properties for WorkflowTimer
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setAlwaysOnTop(true);
         this.setVisible(true);
-        this.setPreferredSize(new Dimension(400, 150));
+        this.setResizable(false);
         this.pack();
 
         //Thread used to keep time
@@ -152,7 +116,9 @@ class WorkflowTimer extends JFrame implements Runnable {
         //Format the name with the time
         name += String.format("\t\t%02d:%02d:%02d", elapsedHours, elapsedMin, elapsedSeconds);
         //Create a JLabel with the name and time
-        JLabel task = new JLabel(name, SwingConstants.RIGHT);
+        JLabel task = new JLabel(name);
+        //Set the label to align to the right
+        task.setAlignmentX(Component.RIGHT_ALIGNMENT);
         //Allow deleting of the label
         task.addMouseListener(new ClickListener());
         //Add task to the task panel
@@ -162,10 +128,39 @@ class WorkflowTimer extends JFrame implements Runnable {
         newTask = true;
     }
 
+    private void onButton(ActionEvent e) {
+        String currentText = ((JButton) e.getSource()).getText();
+        boolean enabled = ((JButton) e.getSource()).isEnabled();
+        if (enabled)
+            switch (currentText) {
+                case "Reset":
+                    theTime = 0;
+                    timeString = "00:00\t\t";
+                    timeText.setText(timeString);
+                    this.timeLeft.setEnabled(false);
+                    taskPanel.removeAll();
+                    taskPanel.add(WorkflowTimer.taskListLabel);
+                    break;
+                case "Lap":
+                    new LapWindow(this);
+                    break;
+                case "Start":
+                    timeLeft.setText("Lap");
+                    timeLeft.setEnabled(true);
+                    timeRight.setText("Stop");
+                    timing = true;
+                    break;
+                case "Stop":
+                    timeLeft.setText("Reset");
+                    timeRight.setText("Start");
+                    timing = false;
+            }
+    }
+
     @Override
     public void run() {
         //Run forever
-        while(true) {
+        while (true) {
             //Continue if timer is not running
             if (!timing) {
                 Thread.yield();
@@ -179,7 +174,7 @@ class WorkflowTimer extends JFrame implements Runnable {
             //Add elapsedTime to theTime
             theTime += elapsedInstanceTime;
             //Initialize lastTaskTime
-            if(newTask) {
+            if (newTask) {
                 lastTaskTime = theTime;
                 newTask = false;
             }
@@ -189,14 +184,14 @@ class WorkflowTimer extends JFrame implements Runnable {
             long elapsedMin = TimeUnit.MILLISECONDS.toMinutes(theTime) % 60;
             long elapsedHours = TimeUnit.MILLISECONDS.toHours(theTime);
             //Find out how many colons we need and format the string as appropriate
-            if(elapsedHours > 0)
+            if (elapsedHours > 0)
                 timeString = String.format("%02d:%02d:%02d:%02d\t\t", elapsedHours, elapsedMin, elapsedSeconds, elapsedMilli);
-            else if(elapsedMin > 0)
+            else if (elapsedMin > 0)
                 timeString = String.format("%02d:%02d:%02d\t\t", elapsedMin, elapsedSeconds, elapsedMilli);
             else
                 timeString = String.format("%02d:%02d\t\t", elapsedSeconds, elapsedMilli);
             //Set the time label's text to the timeString
-            if(timeString.length() != timeText.getText().length()) {
+            if (timeString.length() != timeText.getText().length()) {
                 timeText.setText(timeString);
                 this.pack();
             } else
@@ -213,15 +208,19 @@ class WorkflowTimer extends JFrame implements Runnable {
         }
 
         @Override
-        public void mousePressed(MouseEvent e) {}
+        public void mousePressed(MouseEvent e) {
+        }
 
         @Override
-        public void mouseReleased(MouseEvent e) {}
+        public void mouseReleased(MouseEvent e) {
+        }
 
         @Override
-        public void mouseEntered(MouseEvent e) {}
+        public void mouseEntered(MouseEvent e) {
+        }
 
         @Override
-        public void mouseExited(MouseEvent e) {}
+        public void mouseExited(MouseEvent e) {
+        }
     }
 }
